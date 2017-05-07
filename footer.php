f1180f2b49ccada6f555a81449b2782a273ae5d5
Maya Pasini <mpasini810@gmail.com>
2017-05-07 14:14:46 -0400

    created footer.php and modified index.php

---
+++ footer.php
@@ -0,0 +1,46 @@
+
+
+
+    <footer class="blog-footer">
+
+      <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+        <div class="sidebar-module sidebar-module-inset">
+          <h4>About</h4>
+          <p>By Cristy Yeung. Feel free to share this around but don’t plagiarize or istg I’ll come after you and scrub your face with St. Ives Apricot Scrub and put lemon juice and essential oils on it for all eternity. </p>
+        </div>
+        <div class="sidebar-module">
+          <h4>Archives</h4>
+          <ol class="list-unstyled">
+            <li><a href="http://getbootstrap.com/examples/blog/#">June 2017</a></li>
+          </ol>
+        </div>
+        <div class="sidebar-module">
+          <h4>Elsewhere</h4>
+          <ol class="list-unstyled">
+            <li><a href="http://getbootstrap.com/examples/blog/#">GitHub</a></li>
+            <li><a href="http://getbootstrap.com/examples/blog/#">Twitter</a></li>
+            <li><a href="http://getbootstrap.com/examples/blog/#">Facebook</a></li>
+          </ol>
+        </div>
+      </div><!-- /.blog-sidebar -->
+
+    </div><!-- /.row -->
+
+  </div><!-- /.container -->
+
+      <a href="index.php?page=2">Next page</a>
+
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./Blog Template for Bootstrap_files/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="./Blog Template for Bootstrap_files/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./Blog Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<!-- saved from url=(0038)http://getbootstrap.com/examples/blog/ -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Skincare Auntie</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="./Blog Template for Bootstrap_files/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="./Blog Template for Bootstrap_files/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="./Blog Template for Bootstrap_files/blog.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="./Blog Template for Bootstrap_files/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  <style type="text/css">
-:root #carbonads-container,
-:root #content > #right > .dose > .dosesingle,
-:root #content > #center > .dose > .dosesingle,
-:root .carbonad
-{ display: none !important; }</style></head>
-
 <?php
+
+    require 'header.php';
     require_once 'database.php';
     $mysql = getDB();
 
@@ -97,56 +62,15 @@
             <ul class="pager">
               <?php
                 if ($page_number >= 2) { ?>
-                  <li><a href="blog.php?page=<?php echo $page_number-1; ?>">Previous</a></li>
+                  <li><a href="index.php?page=<?php echo $page_number-1; ?>">Previous</a></li>
               <?php
                 }
               ?>
-              <li><a href="blog.php?page=<?php echo $page_number+1; ?>">Next</a></li>
+              <li><a href="index.php?page=<?php echo $page_number+1; ?>">Next</a></li>
             </ul>
           </nav>
 
         </div><!-- /.blog-main -->
-
-        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-          <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>By Cristy Yeung. Feel free to share this around but don’t plagiarize or istg I’ll come after you and scrub your face with St. Ives Apricot Scrub and put lemon juice and essential oils on it for all eternity. </p>
-          </div>
-          <div class="sidebar-module">
-            <h4>Archives</h4>
-            <ol class="list-unstyled">
-              <li><a href="http://getbootstrap.com/examples/blog/#">June 2017</a></li>
-            </ol>
-          </div>
-          <div class="sidebar-module">
-            <h4>Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li><a href="http://getbootstrap.com/examples/blog/#">GitHub</a></li>
-              <li><a href="http://getbootstrap.com/examples/blog/#">Twitter</a></li>
-              <li><a href="http://getbootstrap.com/examples/blog/#">Facebook</a></li>
-            </ol>
-          </div>
-        </div><!-- /.blog-sidebar -->
-
-      </div><!-- /.row -->
-
-    </div><!-- /.container -->
-
-    <footer class="blog-footer">
-
-      <a href="blog.php?page=2">Next page</a>
-
-    </footer>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./Blog Template for Bootstrap_files/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="./Blog Template for Bootstrap_files/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="./Blog Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
-
-</body>
-</html>
+<?php
+require footer.php
+?>
